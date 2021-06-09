8f59ad89312f8eeacbf642bb0884055e78233b93
Yeoh Lik Hern <yeohlikh@gmail.com>
2021-06-09 21:46:01 +0800

    Formatting

--- bot.js
+++ bot.js
@@ -24,12 +24,11 @@ bot.onText(/^\/schedule ([0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4})$/, async (msg, match)
     const msgId = msg.message_id;
 
     const meetingDate = moment(match[1], 'DD/MM/YYYY').startOf('day');
-    await admin.database().ref().push().set({
-        chatId,
+    await admin.database().ref(chatId).set({
         meetingDate: meetingDate.valueOf(),
     });
 
-    await bot.sendMessage(chatId, `${ sender.username ? '@' + sender.username : '<b>' + sender.first_name + '</b>' } wants to schedule a meeting on <b>${ meetingDate.format('DD MMM YYYY') }</b>.\nPlease indicate your availability in the format <b>HH:MM-HH:MM</b>, separated by linebreaks where applicable.\n<b>Example:</b>\n09:00-11:30\n14:00-16:00`, {
+    await bot.sendMessage(chatId, `${ sender.username ? '@' + sender.username : '<b>' + sender.first_name + '</b>' } wants to schedule a meeting on <b>${ meetingDate.format('DD MMM YYYY') }</b>.\n\nPlease indicate your availability in the format <b>HH:MM-HH:MM</b>, separated by linebreaks where applicable.\n\n<b>Example:</b>\n09:00-11:30\n14:00-16:00`, {
         parse_mode: 'HTML',
         reply_to_message_id: msgId,
     });
