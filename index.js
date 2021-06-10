81184b13e814db41a3de2a64ac983b5eb8c587dd
Yeoh Lik Hern <yeohlikh@gmail.com>
2021-06-10 19:24:02 +0800

    Removed unnecessary files/directories

--- index.js
+++ index.js
@@ -28,19 +28,14 @@ admin.initializeApp({
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res) => {
-    res.render('index', { title: 'Meeting Bot' });
+    res.sendStatus(200);
 });
 
 // catch 404 and forward to error handler
@@ -55,8 +50,7 @@ app.use(function(err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    res.sendStatus(err.status || 500);
 });
 
 /**
